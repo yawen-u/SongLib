13a65e133deae05fb8ff6052d2d922bfa83282ef
yawen-u <80566708+yawen-u@users.noreply.github.com>
2022-02-07 16:05:50 -0500

    Set up
    description

--- src/SongLib.java
+++ src/SongLib.java
@@ -2,31 +2,14 @@
 import java.io.IOException;
 
 import javafx.application.Application;
-// import javafx.event.ActionEvent;
-// import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-// import javafx.scene.control.Button;
-// import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
-public class App extends Application {
+public class SongLib extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // Button btn = new Button();
-        // btn.setText("Say 'Hello World'");
-        // btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-        //     @Override
-        //     public void handle(ActionEvent event) {
-        //         System.out.println("Hello World!");
-        //     }
-        // });
-        
-        // StackPane root = new StackPane();
-        // root.getChildren().add(btn);
-  
 
         Parent root;
         try {
