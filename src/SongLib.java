63deba4e800e1cadc778f29cb19d183db245e496
yawen-u <80566708+yawen-u@users.noreply.github.com>
2022-02-08 15:03:29 -0500

    app

--- src/SongLib.java
+++ src/SongLib.java
@@ -1,32 +1,33 @@
-package App;
-
-
-import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.ListController;
  
 public class SongLib extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+		
+		// set up FXML loader
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/view/list.fxml"));
+		
+		// load the fxml
+		AnchorPane root = (AnchorPane)loader.load();
 
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-            Scene scene = new Scene(root);
+		// get the controller (Do NOT create a new Controller()!!)
+		// instead, get it through the loader
+		ListController listController = loader.getController();
+		listController.start(primaryStage);
 
-            
-            //Scene scene = new Scene(root, 300, 250);
-    
-            primaryStage.setTitle("Hello World!");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch(IOException e) {
-        }
-    }
+		Scene scene = new Scene(root, 200, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
+
+	}
 
     public static void main(String[] args) {
         launch(args);
